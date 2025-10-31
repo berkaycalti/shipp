16f5c72e36e1de3aa414196f3b5c6cf5c7fe398b
shippdateapp <shippdateapp@gmail.com>
2025-10-31 21:57:12 +0000

    feat: Configure Firebase and initialize in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shipp/constants/colors.dart';
-import 'screens/auth/login_screen.dart';
+import 'package:shipp/screens/auth/login_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ShippApp());
 }
 
